3847b0d843059b807f2d309e760d8a7f57a3d63f
Switch <31280229+gavin1970@users.noreply.github.com>
2025-11-12 18:36:40 -0500

    Removing commented code

--- demos/CSharpConsole/Program.cs
+++ demos/CSharpConsole/Program.cs
@@ -20,34 +20,6 @@ namespace CSharpConsole
 
             // with color, use NULL instead for no color.
             anyKey("\n\nPress any key to exit...\n\n", rgbRed);
-
-            //// 1. Convert to Hex
-            //string hexWithAlpha = ColorApi.RgbToRgbHex(rgbAshRose, true);
-            //string hexNoAlpha = ColorApi.RgbToRgbHex(rgbAshRose, false);
-
-            //// 2. Convert to Decimal
-            //int rgbDec = ColorApi.RgbToRgbDec(rgbAshRose);
-            //int argbDec = ColorApi.RgbToArgbDec(rgbAshRose);
-
-            //// 3. Convert to HSL/HSV
-            //HslSpace hsl = ColorApi.RgbToHsl(rgbAshRose);
-            //HsvSpace hsv = ColorApi.RgbToHsv(rgbAshRose);
-
-            //// 4. Convert back to RGB
-            //RgbColor rtHslColor = ColorApi.HslToRgb(hsl);
-            //RgbColor rtHsvColor = ColorApi.HsvToRgb(hsv);
-
-            //// 5. Lets show our results
-            //Console.WriteLine($"Hex (Alpha): {hexWithAlpha}, Dec: {argbDec}");      // e.g., #FF40E0D0
-            //Console.WriteLine($"Hex (No Alpha): {hexNoAlpha}, Dec: {rgbDec}\n");    // e.g., #40E0D0
-
-            //Console.WriteLine($"HSL: H={hsl.hue:F2}, S={hsl.saturation:F2}, L={hsl.lightness:F2}");
-            //Console.WriteLine($"HSV: H={hsv.hue:F2}, S={hsv.saturation:F2}, L={hsv.value:F2}\n");
-
-            //Console.WriteLine($"Roundtrip HSL->RGB: A={rtHslColor.alpha}, R={rtHslColor.red}, G={rtHslColor.green}, B={rtHslColor.blue}");
-            //Console.WriteLine($"Roundtrip HSV->RGB: A={rtHsvColor.alpha}, R={rtHsvColor.red}, G={rtHsvColor.green}, B={rtHsvColor.blue}\n");
-
-            //Console.ReadKey(true);
         }
 
         static void SetColors(RgbColor bg, RgbColor fg)
